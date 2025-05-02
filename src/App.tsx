0165e4991973e13c6f1443f80815f47631141b95
Albert Sanchez <albert.san.iza@gmail.com>
2025-05-02 10:51:07 -0700

    fix: reduce padding in main element for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Map from './map'
 
 export default function App() {
     return (
-        <main className="fixed inset-0 bg-radial from-slate-700 from-40% to-slate-950 p-12">
+        <main className="fixed inset-0 bg-radial from-slate-700 from-40% to-slate-950 p-6">
             <div id="stars"></div>
             <Map />
         </main>
