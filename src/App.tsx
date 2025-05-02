6c7629146b9c27b49dbbe8d03f28a57811c1d110
Albert Sanchez <albert.san.iza@gmail.com>
2025-05-02 01:46:25 -0700

    fix: correct order of class names in main element for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Map from './map'
 
 export default function App() {
     return (
-        <main className="fixed bg-slate-900 inset-0 p-6">
+        <main className="fixed inset-0 bg-slate-900 p-6">
             <Map />
         </main>
     )
