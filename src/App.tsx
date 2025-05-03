af3f5d3e8205cac980f44e5916f382fb97c320d4
Albert Sanchez <albert.san.iza@gmail.com>
2025-05-03 10:33:43 -0700

    fix: enhance header styling for improved visibility and consistency

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,13 @@ export default function App() {
             <div id="stars"></div>
             <div className="pointer-events-none absolute top-0 flex w-full items-center justify-between px-6 pt-6">
                 <div>
-                    <h1 className="w-fit rounded-xl bg-black/20 px-2 py-1 text-6xl font-semibold tracking-tight text-white backdrop-blur-xs text-shadow-lg">
+                    <h1
+                        className={`w-fit ${started && !finished ? 'rounded-t-xl' : 'rounded-xl'} bg-black/20 px-2 py-1 text-6xl font-semibold tracking-tight text-white backdrop-blur-sm text-shadow-lg`}
+                    >
                         World Atlas
                     </h1>
                     {started && !finished && (
-                        <div className="rounded-r-xl rounded-b-xl bg-black/20 px-2 py-0 font-mono text-lg font-semibold text-white text-shadow-lg">
+                        <div className="rounded-r-xl rounded-b-xl bg-black/20 px-2 py-0 font-mono text-lg font-semibold text-white backdrop-blur-sm text-shadow-lg">
                             Found: {answeredCount < 10 && <span className="opacity-0">0</span>}
                             {Object.values(answered).filter((value) => value === 'correct').length}/{countries.length}, Remaining:{' '}
                             {countries.length - answeredCount < 10 && <span className="opacity-0">0</span>}
