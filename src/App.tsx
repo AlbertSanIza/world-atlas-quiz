b84c8423d4e2be721032d7e2319a7e588856c53f
Albert Sanchez <albert.san.iza@gmail.com>
2025-05-03 10:15:48 -0700

    fix: move title element inside main for better structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ export default function App() {
     return (
         <main className="fixed inset-0 bg-radial from-slate-700 from-40% to-slate-950">
             <div id="stars"></div>
+            <h1 className="absolute top-6 left-6 flex flex-col rounded-xl bg-black/10 px-2 py-1 text-6xl font-semibold tracking-tight text-white backdrop-blur-xs text-shadow-lg">
+                WORLD ATLAS
+            </h1>
             <Map />
         </main>
     )
