e07f46faf84640592740382dde3e2db34eb09137
Albert Sanchez <albert.san.iza@gmail.com>
2025-05-03 10:30:20 -0700

    fix: restructure header for improved layout and accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
+import ElapsedTime from './elapsed-time'
+import { useGameStore } from './lib/store'
 import Map from './map'
 
 export default function App() {
+    const { started, finished, current, answered, countries, startGame, resetGame } = useGameStore()
+    const answeredCount = Object.values(answered).length
+
     return (
         <main className="fixed inset-0 bg-radial from-slate-700 from-40% to-slate-950">
             <div id="stars"></div>
-            <h1 className="absolute top-6 left-6 flex flex-col rounded-xl bg-black/10 px-2 py-1 text-6xl font-semibold tracking-tight text-white backdrop-blur-xs text-shadow-lg">
-                WORLD ATLAS
-            </h1>
+            <div className="pointer-events-none absolute top-0 flex w-full items-center justify-between px-6 pt-6">
+                <div>
+                    <h1 className="w-fit rounded-xl bg-black/20 px-2 py-1 text-6xl font-semibold tracking-tight text-white backdrop-blur-xs text-shadow-lg">
+                        World Atlas
+                    </h1>
+                    {started && !finished && (
+                        <div className="rounded-r-xl rounded-b-xl bg-black/20 px-2 py-0 font-mono text-lg font-semibold text-white text-shadow-lg">
+                            Found: {answeredCount < 10 && <span className="opacity-0">0</span>}
+                            {Object.values(answered).filter((value) => value === 'correct').length}/{countries.length}, Remaining:{' '}
+                            {countries.length - answeredCount < 10 && <span className="opacity-0">0</span>}
+                            {countries.length - answeredCount}, Time: <ElapsedTime />
+                        </div>
+                    )}
+                </div>
+                <div className="pointer-events-auto">
+                    {!started ? (
+                        <button
+                            className="flex h-10 w-18 items-center justify-center rounded-lg bg-black text-white hover:cursor-pointer hover:opacity-80"
+                            onClick={startGame}
+                        >
+                            Start
+                        </button>
+                    ) : (
+                        <button
+                            className="flex h-10 w-18 items-center justify-center rounded-lg border-2 hover:cursor-pointer hover:underline hover:opacity-80"
+                            onClick={resetGame}
+                        >
+                            Reset
+                        </button>
+                    )}
+                </div>
+            </div>
             <Map />
         </main>
     )
